b79e7ef69ba76c84e16e30e2c9411d52dc919996
prs1234 <93731652+prs1234@users.noreply.github.com>
2021-11-04 23:17:51 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,25 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightNav = document.querySelector('.rightnav')
+navlist = document.querySelector('.nav-list')
+
+
+burger.addEventListener('click',()=>{
+
+rightNav.classList.toggle('v-class');
+navlist.classList.toggle('v-class');
+navbar.classList.toggle('h-nav');
+
+
+
+
+
+
+})
+btn.addEventListener('click', function(){
+
+document.getElementById('btn').style.background="rgb(85, 79, 79)";
+document.querySelector('#btn').innerHTML="<b> Subscribed <b>"
+
+
+})
